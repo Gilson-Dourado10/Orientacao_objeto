86afa17e8c1e2735214e57ad26840a08c8067d79
Gilson Batista de Almeida Dourado <gilsondourado10@gmail.com>
2023-12-30 13:38:16 -0300

    feat(configuração)criando a primeira classe

---
+++ Program.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Orientacao_objetos
+{
+    public class OrientacaoObjeto
+    {
+       public static void Main()
+       {
+        
+       } 
+    }
+}
