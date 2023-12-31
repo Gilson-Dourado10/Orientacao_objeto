e30a8b7f29fa25d486c19f8660dbab88af57f873
Gilson Batista de Almeida Dourado <gilsondourado10@gmail.com>
2023-12-31 06:26:16 -0300

    feat(Classes) criando a class Carro

---
+++ Carro.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Orientacao_objetos
+{
+    public class Carro
+    {
+        public string Modelo;
+        public string Cor;
+        public int ANO;
+
+        public void Ligar()
+        {
+            Console.WriteLine("Carro ligado");
+
+        }
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,15 +1,5 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace Orientacao_objetos
-{
-    public class OrientacaoObjeto
-    {
-       public static void Main()
+    static void Main()
        {
         
        } 
-    }
-}
+    
