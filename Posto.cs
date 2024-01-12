dac09f64e67b865a66918f9338909a1f91280ce2
Gilson Batista de Almeida Dourado <gilsondourado10@gmail.com>
2024-01-12 07:31:06 -0300

    feat(abstração/encapsulamento)criando a classe posto com propriedades e métodos

---
+++ Posto.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Orientacao_objetos
+{
+    public class Posto
+    {
+        private string Proprietario;
+        private int ValorLitro = 2;
+        private string Modelo;
+        public int Tanque { get; set ; }
+        public static int VeiculosAbastecidos{get; private set; }
+
+    }
+}
