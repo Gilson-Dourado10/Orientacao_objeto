f6b305a900f8c5e7a32024d660dbb78ccb7743e5
Gilson Batista de Almeida Dourado <gilsondourado10@gmail.com>
2024-01-13 07:24:13 -0300

    feat(abstração/encapsulamento) instanciando as classes

--- Program.cs
+++ Program.cs
@@ -1,1 +1,16 @@
 
+using Orientacao_objetos;
+Posto carro1 = new Posto("Geraldo");
+carro1.Modelo = "Corsa hatch";
+carro1.Tanque = 45;
+carro1.Abastecer(90); 
+Console.WriteLine("Litros de combustível: "+ carro1.Tanque);
+
+Posto carro2 = new Posto("Antônio");
+carro2.Modelo = "Celta";
+carro2.Tanque = 40;
+carro2.Abastecer(20);
+Console.WriteLine("Litros combustível: "+ carro2.Tanque);
+
+Console.Write("quantidades veículos abastecidos: ");
+Console.WriteLine(Posto.VeiculosAbastecidos);
