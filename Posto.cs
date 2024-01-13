53f7d6c4e7bf05afff2e86937579f470937e08ee
Gilson Batista de Almeida Dourado <gilsondourado10@gmail.com>
2024-01-13 07:00:25 -0300

    feat(abstração/encapsulamento) refatorando variáveis privadas

--- Posto.cs
+++ Posto.cs
@@ -1,20 +1,33 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace Orientacao_objetos
 {
     public class Posto
     {
-        private string Proprietario;
-        private int ValorLitro = 2;
-        private string Modelo;
-        public int Tanque { get; set ; }
-        public static int VeiculosAbastecidos{get; private set; }
-    }
-    public Posto(string proprietario)
-    {
-        Proprietario = proprietario;
+        private string _proprietario;
+        private int _valorLitro = 2;
+        private string _modelo;
+        public int Tanque { get; set; }
+        public static int VeiculosAbastecidos { get; private set; }
+
+        public Posto(string proprietario)
+        {
+            _proprietario = proprietario;
+        }
+        public string Modelo
+        {
+            get { return _modelo; }
+            set
+            {
+                if (value != null && value.Length > 1)
+                {
+                    _modelo = value;
+                }
+            }
+        }
+        public void Abastecer(int valor)
+        {
+            Tanque = valor / _valorLitro;
+            VeiculosAbastecidos++;
+        }
     }
 }
