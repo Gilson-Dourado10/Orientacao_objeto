d1630c557c2b5ace56274e9ca20e110c8ff6c5e1
Gilson Batista de Almeida Dourado <gilsondourado10@gmail.com>
2024-01-16 06:01:39 -0300

    feat(abstração/encapsulamento) chamando o construtor base

--- Carreta.cs
+++ Carreta.cs
@@ -1,10 +1,12 @@
-using System;
+
 
 
 namespace Orientacao_objetos;
 
     public class Carreta : Veiculo
     {
+      public Carreta(string proprietario) : base(proprietario){}
+      
        public bool Carroceria = true;
        public override double Rodas()
        {
