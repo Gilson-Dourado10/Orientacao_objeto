6134a93497c268c202c310eda4a8ceb400ed787a
Gilson Batista de Almeida Dourado <gilsondourado10@gmail.com>
2024-01-19 05:24:49 -0300

    feat(herança) pagamento com cartão crédito

--- PagamentoCartaoCredito.cs
+++ PagamentoCartaoCredito.cs
@@ -3,6 +3,12 @@ namespace Orientacao_objetos;
 
     public class PagamentoCartaoCredito : Pagamento
     {
-       public string  numero;
+       public string  NumeroCartao;
+    public override void Pagar()
+    {
+        Console.WriteLine("Iniciando o pagamento com cartão de crédito");
+        Console.WriteLine($"Pagamento com cartão de crédito {NumeroCartao} realizado ");
+        base.Pagar(); // chamando o método  Pagar() da classe base
     }
+}
 
--- Program.cs
+++ Program.cs
@@ -4,9 +4,16 @@ var pagamentoBoleto = new PagamentoBoleto();
 pagamentoBoleto.Vencimento = DateTime.Now;
 pagamentoBoleto.NumeroBoleto = "123.555-55-99999";
 
+Console.WriteLine("****Pagamento Boleto****");
 Console.WriteLine($"Número do Boleto: {pagamentoBoleto.NumeroBoleto}");
 Console.WriteLine($"Vencimento Boleto: {pagamentoBoleto.Vencimento}");
-Console.WriteLine("****Pagamento Boleto****");
 pagamentoBoleto.Pagar();
 //***************************************************************
+var pagamentoCartaoCredito = new PagamentoCartaoCredito();
+pagamentoCartaoCredito.NumeroCartao = "123.4566.589";
+pagamentoCartaoCredito.Vencimento = DateTime.Now;
 Console.WriteLine();
+Console.WriteLine("***********pagamento Cartão crédito*********");
+Console.WriteLine($"Número do Cartão crédito: {pagamentoCartaoCredito.NumeroCartao }");
+Console.WriteLine($"Vencimento fatura cartão: {pagamentoBoleto.Vencimento}");
+pagamentoCartaoCredito.Pagar();
