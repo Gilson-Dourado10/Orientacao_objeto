3c958619ae42ec3ec4dec8f79f11b18e227194d9
Gilson Batista de Almeida Dourado <gilsondourado10@gmail.com>
2024-01-20 09:00:30 -0300

    feat(polimorfismo) classe Professor herdando de membroUniversidade

--- Professor.cs
+++ Professor.cs
@@ -1,14 +1,14 @@
 
 
-// namespace Orientacao_objetos;
+namespace Orientacao_objetos;
 
-// public class Professor : MembroUniversidade
-// {
-//     public string Disciplina { get; set; }
+public class Professor : MembroUniversidade
+{
+    public string Disciplina { get; set; }
 
-//     public override void ExibirInfo()
-//     {
-//         Console.WriteLine($"Nome: {Nome} e Disciplina: {Disciplina} ");
-//         base.ExibirInfo();
-//     }
-// }
+    public override void ExibirInfo()
+    {
+        Console.WriteLine($"Nome: {Nome} e Disciplina: {Disciplina} ");
+        base.ExibirInfo();
+    }
+}
