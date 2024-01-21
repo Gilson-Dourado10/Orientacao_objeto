3511463767e279c63bef30741027ec7431ca787a
Gilson Batista de Almeida Dourado <gilsondourado10@gmail.com>
2024-01-21 06:46:36 -0300

    feat(polimorfismo) propriedade Datetime adicionada

--- Aluno.cs
+++ Aluno.cs
@@ -5,6 +5,7 @@ namespace Orientacao_objetos;
     public class Aluno : MembroUniversidades
     {
      public string Curso{get ; set ;}
+     public DateTime InicioCurso{ get; set ;}
     public override void ExibirInfo()
     {
         Console.WriteLine($"Nome Aluno: {Nome} Curso: {Curso}");
