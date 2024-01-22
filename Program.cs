8572525954adc2c7804002dd1c7818b8e7658977
Gilson Batista de Almeida Dourado <gilsondourado10@gmail.com>
2024-01-22 06:30:03 -0300

    feat(Modificador de acesso/Public) modificador de acesso public

--- Program.cs
+++ Program.cs
@@ -1,14 +1,44 @@
-using Orientacao_objetos;
-Console.Clear();
+// using Orientacao_objetos;
+// Console.Clear();
 
-List<MembroUniversidades> membroUniversidades = new List<MembroUniversidades>();
-membroUniversidades.Add(new Professor {Nome = "Marco Castro" , Disciplina = "Orientação Objetos" , DataProvaFinalDisciplina = new DateTime(2024,1, 25, 19, 30, 0)}); 
-membroUniversidades.Add(new Aluno {Nome = "Felipe Ricardo" , Curso = "Análise e Desenvolvimento de sistemas", InicioCurso = new DateTime(2023,05,3)});
-membroUniversidades.Add(new Professor {Nome = "Adão Santos" , Disciplina = "Power BI"});
-membroUniversidades.Add(new Aluno {Nome = "João Pedro" , Curso = "Sistema para Internet"});
+// List<MembroUniversidades> membroUniversidades = new List<MembroUniversidades>();
+// membroUniversidades.Add(new Professor {Nome = "Marco Castro" , Disciplina = "Orientação Objetos" , DataProvaFinalDisciplina = new DateTime(2024,1, 25, 19, 30, 0)}); 
+// membroUniversidades.Add(new Aluno {Nome = "Felipe Ricardo" , Curso = "Análise e Desenvolvimento de sistemas", InicioCurso = new DateTime(2023,05,3)});
+// membroUniversidades.Add(new Professor {Nome = "Adão Santos" , Disciplina = "Power BI"});
+// membroUniversidades.Add(new Aluno {Nome = "João Pedro" , Curso = "Sistema para Internet"});
 
-foreach (var membro in membroUniversidades )
+// foreach (var membro in membroUniversidades )
+// {
+//  membro.ExibirInfo();   
+// }
+
+using System;
+
+public class Exemplo
 {
- membro.ExibirInfo();   
+    // Membro público
+    public string MensagemPublica = "Isso é público.";
+
+    // Método público
+    public void ExibirMensagem()
+    {
+        Console.WriteLine(MensagemPublica);
+    }
 }
 
+class Program
+{
+    static void Main()
+    {
+        // Instanciando a classe
+        Exemplo meuExemplo = new Exemplo();
+
+        // Acessando e exibindo o membro público
+        Console.WriteLine($"Mensagem: {meuExemplo.MensagemPublica}");
+
+        // Chamando o método público
+        meuExemplo.ExibirMensagem();
+
+        Console.ReadLine();
+    }
+}
