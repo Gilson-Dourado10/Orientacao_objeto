67903d9b66fe60274e167efc76df73e4e5e39e8b
Gilson Batista de Almeida Dourado <gilsondourado10@gmail.com>
2024-01-23 06:37:20 -0300

    feat(Propriedades) usando propriedades automáticas públicas

---
+++ MinhaPropriedade.cs
@@ -0,0 +1,17 @@
+
+
+namespace Orientacao_objetos;
+
+    public class MinhaPropriedade
+    {
+        public int MyProperty { get; set; }
+    }
+
+
+// namespace Orientacao_objetos;
+//     public class MyProperty
+//     {
+//     private int myProperty;
+
+//     public int MyProperty { get => myProperty; set => myProperty = value; }
+// }
