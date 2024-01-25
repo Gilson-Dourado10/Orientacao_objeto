2d46340628eacb223dcf06dd203a85c46a986cd2
Gilson Batista de Almeida Dourado <gilsondourado10@gmail.com>
2024-01-25 07:07:53 -0300

    feat(Propriedades) propriedade automática normal get e set

--- MinhaPropriedade.cs
+++ MinhaPropriedade.cs
@@ -1,34 +1,56 @@
 
 
-// namespace Orientacao_objetos;
+namespace Orientacao_objetos;
 
-//     public class MinhaPropriedade
+    public class MinhaPropriedade
+    {
+        public int MyProperty { get; set; }
+    }
+//****************************************************************************
+// public class Minhapropiriedade
+// {
+//     private double _myProperty;
+//     public double MyProperty
 //     {
-//         public int MyProperty { get; set; }
+//         get {return _myProperty; }
+//         set {_myProperty = value;}
 //     }
+// }
 
 
-// namespace Orientacao_objetos;
+//********************************************************************************
+
 //     public class MyProperty
 //     {
-//     private int myProperty;
+//     private decimal _myPropertyy;
 
-//     public int MyProperty { get => myProperty; set => myProperty = value; }
+//     public decimal MyPropertyy 
+
+//     { 
+//         get => _myPropertyy;
+//         set => _myPropertyy = value;
+//     }
 // }
+
+
+
+
+
+
 
 
 //*******************************************************************
 
-namespace Orientacao_objetos;
-public class MinhaPropriedade
-{
-    public int MyProperty { get; private set; }
-    public MinhaPropriedade(int valorInicial)
-    {
-        MyProperty = valorInicial;
-    }
-    public void AlterarValorDaPropriedade(int NovoValor)
-    {
-        MyProperty = NovoValor;
-    }
-}
+// namespace Orientacao_objetos;
+// public class MinhaPropriedade
+// {
+//     public int MyProperty { get; private set; }
+//     public MinhaPropriedade(int valorInicial)
+//     {
+//         MyProperty = valorInicial;
+//     }
+//     public void AlterarValorDaPropriedade(int NovoValor)
+//     {
+//         MyProperty = NovoValor;
+//     }
+// }
