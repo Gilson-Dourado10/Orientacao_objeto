b106a5d5d91333e49197f9445fb9fbb12ca2903e
Gilson Batista de Almeida Dourado <gilsondourado10@gmail.com>
2024-01-27 08:28:45 -0300

    feat(get/leitura)criando a classe futebol com um construtor

---
+++ Futebol.cs
@@ -0,0 +1,14 @@
+
+
+namespace Orientacao_objetos;
+
+    public class Futebol
+    {
+       public string Time {get; private set;  } 
+
+       public Futebol(string timeSerieA)
+       {
+            Time = timeSerieA;
+       }
+       
+    }
--- Program.cs
+++ Program.cs
@@ -1,35 +1,4 @@
-// using Orientacao_objetos;
-
-// Console.Clear();
-// var minhaPropriedade = new MinhaPropriedade();
-// minhaPropriedade.MyProperty = 40;
-// Console.WriteLine($" O valor da propriedade : {minhaPropriedade.MyProperty}");
-
-//******************************************************************************************
-
-// using Orientacao_objetos;
-
-// Console.Clear();
-// var minhaPropriedade = new MinhaPropriedade(45);
-// var primeiroValor = minhaPropriedade.MyProperty;
-// Console.WriteLine($"Primeiro valor da propriedade: {primeiroValor}");
-// Console.WriteLine();
-// minhaPropriedade.AlterarValorDaPropriedade(50);
-// var segundoValor = minhaPropriedade.MyProperty;
-// Console.WriteLine($"Segundo valor da propriedade: {segundoValor}");
-
-
-//************************************************************************************************
-using Orientacao_objetos;
-
- Console.Clear();
-
-var person = new ClassPerson("Everaldo dos santos");
-var nomeDaPessoa = person.Nome;
-Console.WriteLine($"Nome da pessoa: {nomeDaPessoa}");
- person.AlterarNomePessoa("Edicleuso da Silva");
- var novoNomePessoa = person.Nome;
- Console.WriteLine($"Novo nome da Pessoa: {novoNomePessoa}");
+ using Orientacao_objetos;
 
 
 
@@ -40,12 +9,3 @@ Console.WriteLine($"Nome da pessoa: {nomeDaPessoa}");
 
 
 
-
-
-
-// MinhaPropriedad minhaPropriedad = new MinhaPropriedad();
-// minhaPropriedad.ValorCestaBasica = 300;
-// int precoCestaBasica = minhaPropriedad.ValorCestaBasica;
-// Console.WriteLine($"Preço Cesta básica R$: {precoCestaBasica} reais ");
-// Console.WriteLine();
-//***********************************************************************************
