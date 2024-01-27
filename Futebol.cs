fb5930397c845fd53b7dadd8dba9d3686277481d
Gilson Batista de Almeida Dourado <gilsondourado10@gmail.com>
2024-01-27 08:38:05 -0300

    feat(get/leitura)instanciando a classee mostrando valores

--- Futebol.cs
+++ Futebol.cs
@@ -5,10 +5,14 @@ namespace Orientacao_objetos;
     public class Futebol
     {
        public string Time {get; private set;  } 
-
+      // construtor da classe
        public Futebol(string timeSerieA)
        {
             Time = timeSerieA;
        }
-       
+       // Método para alterar o  time série A
+       public string AlterarNomeTime(String novoTimeSeireA)
+       {
+           return  Time = novoTimeSeireA;
+       }
     }
--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
  using Orientacao_objetos;
+Futebol jogo = new Futebol("Palmeiras");
+Console.WriteLine($"Time de futebol: {jogo.Time}");
 
 
 
