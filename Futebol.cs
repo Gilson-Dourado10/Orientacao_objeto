a091611f34b60fe86a922f06a0904edd31707963
Gilson Batista de Almeida Dourado <gilsondourado10@gmail.com>
2024-01-29 07:05:47 -0300

    feat(get/retornar)Criando o método para definor o definir e obter o time

--- Futebol.cs
+++ Futebol.cs
@@ -1,18 +1,32 @@
 
+            // set private 
+// namespace Orientacao_objetos;
 
+//     public class Futebol
+//     {
+//        public string Time {get; private set;  } 
+//       // construtor da classe
+//        public Futebol(string timeSerieA)
+//        {
+//             Time = timeSerieA;
+//        }
+//        // Método para alterar o  time série A
+//        public string AlterarNomeTime(String novoTimeSerieA)
+//        {
+//            return  Time = novoTimeSerieA;
+//        }
+//     }
+//************************************************************************************
 namespace Orientacao_objetos;
-
-    public class Futebol
+public class Futeboll
+{
+    public string Time {private get ; set; }
+    public void DefinirTime(string novoTime)
     {
-       public string Time {get; private set;  } 
-      // construtor da classe
-       public Futebol(string timeSerieA)
-       {
-            Time = timeSerieA;
-       }
-       // Método para alterar o  time série A
-       public string AlterarNomeTime(String novoTimeSerieA)
-       {
-           return  Time = novoTimeSerieA;
-       }
+        Time = novoTime;
     }
+    public String ObterTime()
+    {
+        return Time;
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,12 +1,22 @@
- using Orientacao_objetos;
- Console.Clear();
-Futebol jogo = new Futebol("Palmeiras");
-Console.WriteLine($"Time de futebol: {jogo.Time}");
-// tentando alterar o nome do time fora da clase, vai dar erro de compilaçao. 
-//jogo.Time = "Corithians";
-jogo.AlterarNomeTime("São Paulo");
-Console .WriteLine($"Novo time de futebol : {jogo.Time}");
+//                      set private    
 
+//  using Orientacao_objetos;
+//  Console.Clear();
+// Futebol jogo = new Futebol("Palmeiras");
+// Console.WriteLine($"Time de futebol: {jogo.Time}");
+// // tentando alterar o nome do time fora da clase, vai dar erro de compilaçao. 
+// //jogo.Time = "Corithians";
+// jogo.AlterarNomeTime("São Paulo");
+// Console .WriteLine($"Novo time de futebol : {jogo.Time}");
+
+//*****************************************************************************************************
+//get private
+
+using Orientacao_objetos;
+
+Futeboll futeboll = new Futeboll();
+
+Console.WriteLine();                   
 
 
 
