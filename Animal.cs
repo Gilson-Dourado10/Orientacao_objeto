ee64ec9b3cde1cf18ec6dc520000abd34d28771e
Gilson Batista de Almeida Dourado <gilsondourado10@gmail.com>
2024-02-03 10:09:11 -0300

    feat(Metodos)mesmo método com parâmetros diferentes

---
+++ Animal.cs
@@ -0,0 +1,9 @@
+
+namespace Orientacao_objetos;
+
+    public class Animal
+    {
+        public void Andar(string nomeAnimal){}
+        public void Andar(string nomeAnimal , DateTime diaDoNascimento){}
+        
+    }
