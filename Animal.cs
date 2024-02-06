4e5cd81aeb09b05ce44d5129a89289df589ef115
Gilson Batista de Almeida Dourado <gilsondourado10@gmail.com>
2024-02-05 22:01:03 -0300

    feat(Metodos) retornando os valores nos métodos

--- Animal.cs
+++ Animal.cs
@@ -3,7 +3,10 @@ namespace Orientacao_objetos;
 
     public class Animal
     {
-        public virtual void Andar(string nomeAnimal){}
+        public virtual string Andar(string nomeAnimal)
+        {
+            return $"O animal {nomeAnimal} está andando";
+        }
         public virtual  void Andar(string nomeAnimal , DateTime diaDoNascimento){}
         public virtual void Andar(string nomeAnimal, DateTime diaDoNascimento, string raca = "viralata"){} 
     }
