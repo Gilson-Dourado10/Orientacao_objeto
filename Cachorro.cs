87020b5f65e262dff38bd5f5a95c49da933a9103
Gilson Batista de Almeida Dourado <gilsondourado10@gmail.com>
2024-02-07 06:12:05 -0300

    feat(Metodos) usando override para modificar os métodos

--- Cachorro.cs
+++ Cachorro.cs
@@ -1,8 +1,22 @@
 
 
+
 namespace Orientacao_objetos;
 
     public class Cachorro : Animal
     {
-        
+        public string DonoDoAnimal { get; set; }
+    public override string Andar(string nomeAnimal)
+    {
+        return base.Andar(nomeAnimal) + " Cachorro";
     }
+    public override string Andar(string nomeAnimal, DateTime diaDoNascimento)
+    {
+        return base.Andar(nomeAnimal, diaDoNascimento) + " Cachorro";
+    }
+
+    public override string Andar(string nomeAnimal, DateTime diaDoNascimento, string raca = "viralata")
+    {
+        return base.Andar(nomeAnimal, diaDoNascimento, raca) + " Cachorro";
+    }
+}
