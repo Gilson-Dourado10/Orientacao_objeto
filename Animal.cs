079f659419eb174014f32a5ca2c2ed4460318c7d
Gilson Batista de Almeida Dourado <gilsondourado10@gmail.com>
2024-02-06 21:02:48 -0300

    feat(Metodos) descobrindo a idade do animal

--- Animal.cs
+++ Animal.cs
@@ -7,6 +7,10 @@ namespace Orientacao_objetos;
         {
             return $"O animal {nomeAnimal} está andando";
         }
-        public virtual  void Andar(string nomeAnimal , DateTime diaDoNascimento){}
+        public virtual  string Andar(string nomeAnimal , DateTime diaDoNascimento)
+        {
+          int idade = (int)((DateTime.Now - diaDoNascimento).TotalDays / 365);  
+          return $"O animal {nomeAnimal} está andando. Idade: {idade} anos.";
+        }
         public virtual void Andar(string nomeAnimal, DateTime diaDoNascimento, string raca = "viralata"){} 
     }
