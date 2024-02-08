9f8bb0e81ca1ec50341c5223d56a6e033534c7df
Gilson Batista de Almeida Dourado <gilsondourado10@gmail.com>
2024-02-08 05:33:15 -0300

    feat(Metodos) mostrando os valores dos métodos reescritos no main

--- Program.cs
+++ Program.cs
@@ -1,4 +1,15 @@
 using Orientacao_objetos;
+Console.Clear();
 var cachorro = new Cachorro();
 string mensagemCachorro1 = cachorro.Andar("Mell");
-Console.WriteLine(mensagemCachorro1);
+Console.WriteLine($"(1°)  {mensagemCachorro1}");
+
+Console.WriteLine();
+
+DateTime dataNascimentoCachorro = new DateTime(2019,3,25);
+string mensagemCachorro2 = cachorro.Andar("Rex", dataNascimentoCachorro);
+Console.WriteLine($"(2°)  {mensagemCachorro2}");
+
+Console.WriteLine();
+
+DateTime dataNascimentoCachorro3 = new DateTime(2015,05,05);
