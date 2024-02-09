e1548d5175dad40759843999cb17fb8cc2c705ba
Gilson Batista de Almeida Dourado <gilsondourado10@gmail.com>
2024-02-09 06:57:14 -0300

    feat(Metodos)usando o  operador ternário

--- Cachorro.cs
+++ Cachorro.cs
@@ -1,6 +1,8 @@
 
 
 
+using System.Reflection.Metadata.Ecma335;
+
 namespace Orientacao_objetos;
 
     public class Cachorro : Animal
@@ -15,15 +17,18 @@ namespace Orientacao_objetos;
         return base.Andar(nomeAnimal, diaDoNascimento) + " é um  Cachorro";
     }
 
-    public override string Andar(string nomeAnimal, DateTime diaDoNascimento, string raca = "viralata")
+    public override string Andar(string nomeAnimal, DateTime diaDoNascimento, string raca = "poodle")
     {
-        if (raca != "viralata")
-        {
-            return base.Andar(nomeAnimal, diaDoNascimento, raca) + " é um  Cachorro  ";
-        }
-        else
-        {
-            return base.Andar(nomeAnimal, diaDoNascimento, raca);
-        }
+        // if (raca != "viralata")
+        // {
+        //     return base.Andar(nomeAnimal, diaDoNascimento, raca) + " é um  Cachorro  ";
+        // }
+        // else
+        // {
+        //     return base.Andar(nomeAnimal, diaDoNascimento, raca);
+        // }
+
+        // condição ? expressão1_se_true : expressão2_se_false
+        return raca != "viralata" ?  base.Andar(nomeAnimal, diaDoNascimento, raca) + ($"{raca}") : base.Andar(nomeAnimal, diaDoNascimento, raca);
     }
 }
