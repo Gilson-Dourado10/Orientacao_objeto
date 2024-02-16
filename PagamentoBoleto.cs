c6556f7f733f65a2084acfccb205095d70023c8a
Gilson Batista de Almeida Dourado <gilsondourado10@gmail.com>
2024-02-16 18:56:32 -0300

    feat(Metodos) adicionando uma propriedade

--- PagamentoBoleto.cs
+++ PagamentoBoleto.cs
@@ -5,10 +5,14 @@ using System.Threading.Tasks;
 
 namespace Orientacao_objetos;
 
+
 public class PagamentoBoleto : Pagamentos
 {
     public PagamentoBoleto(DateTime vencimento) : base(vencimento)
-    {
+    {   
         public int NumeroBoleto { get; set; }
+        public string NomeRecebedor{get; set; }
+
     }
 }
+
