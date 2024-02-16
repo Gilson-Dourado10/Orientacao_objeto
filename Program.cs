35c1de00b9660a1fa3a9aaa9a38c53b3c20cd0a1
Gilson Batista de Almeida Dourado <gilsondourado10@gmail.com>
2024-02-15 22:35:10 -0300

    feat(Metodos) instanciando a classe

--- Program.cs
+++ Program.cs
@@ -2,3 +2,7 @@ using Orientacao_objetos;
 
 PagarCartao pagarCartao = new PagarCartao(new DateTime(2024,2,13));
 pagarCartao.Pagar();
+
+PagamentoBoleto pagamentoBoleto = new PagamentoBoleto(new DateTime(2023,02,02));
+pagamentoBoleto.NumeroBoleto = 123156465;
+Console.WriteLine();
