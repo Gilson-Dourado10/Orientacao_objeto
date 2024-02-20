f46f7f0ce3658dfbabe5414fe9b629c1d50b6166
Gilson Batista de Almeida Dourado <gilsondourado10@gmail.com>
2024-02-20 09:03:13 -0300

    feat(Metodos) chamando o método pagar no main

--- Program.cs
+++ Program.cs
@@ -3,8 +3,10 @@ using Orientacao_objetos;
 PagarCartao pagarCartao = new PagarCartao(new DateTime(2024,2,13));
 pagarCartao.Pagar();
 
-PagamentoBoleto pagamentoBoleto = new PagamentoBoleto(new DateTime(2023,02,02));
+PagamentoBoleto pagamentoBoleto = new PagamentoBoleto(new DateTime(2023,02,02),1265498742552,"Manoel pereira");
 pagamentoBoleto.NumeroBoleto = 123156465;
 Console.WriteLine();
 
 PagamentoBoleto pagamentoBoleto1 = new PagamentoBoleto(new DateTime(2024,06,01), 12654987422,"Manoel pereira");
+
+pagamentoBoleto1.Pagar();
