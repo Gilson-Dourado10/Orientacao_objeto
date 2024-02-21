2da383b6363c362e49e5a2a8ecb5a65ecbda9aab
Gilson Batista de Almeida Dourado <gilsondourado10@gmail.com>
2024-02-21 11:06:25 -0300

    feat(Metodos) Correção na propriedade do construtor

---
+++ PagamentoDebito.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Orientacao_objetos
+{
+    public class PagamentoDebito
+    {
+        public decimal ValorPago{get ; set;}
+        public string NomeCliente{get; set; }
+    }
+}
