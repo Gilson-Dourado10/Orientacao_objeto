6b1aea9819e26da8c0b640fd7f53a0c9ab937608
Gilson Batista de Almeida Dourado <gilsondourado10@gmail.com>
2024-02-22 18:27:39 -0300

    feat(Metodos) instancia as propriedades

--- Program.cs
+++ Program.cs
@@ -12,8 +12,10 @@ PagamentoBoleto pagamentoBoleto1 = new PagamentoBoleto(new DateTime(2024,06,01),
 pagamentoBoleto1.Pagar();
 Console.WriteLine();
 PagamentoDebito pagamentoDebito = new PagamentoDebito();
-
-
+var nomeCliente = pagamentoDebito.NomeCliente = "João";
+var valorPago=  pagamentoDebito.ValorPago = 300;
+Console.WriteLine($"nome cliente {nomeCliente }");
+Console.WriteLine($"valor pago débito {valorPago}");
 
 Console.WriteLine();
 
