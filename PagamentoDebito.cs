a63eea5c66b0cbb12b6850dab1624f8f7c3eb104
Gilson Batista de Almeida Dourado <gilsondourado10@gmail.com>
2024-02-23 17:52:25 -0300

    feat(Metodos) acrescentando um construtor no método

--- PagamentoDebito.cs
+++ PagamentoDebito.cs
@@ -5,8 +5,13 @@ using System.Threading.Tasks;
 
 namespace Orientacao_objetos
 {
-    public class PagamentoDebito 
+    public class PagamentoDebito : Pagamentos
+    
     {
+        public PagamentoDebito(DateTime vencimento) : base(vencimento)
+        {
+        }
+
         public decimal ValorPago{get ; set;}
         public string NomeCliente{get; set; }
     }
