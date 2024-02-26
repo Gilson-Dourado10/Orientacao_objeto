15bf856d18cdd39b47e59f7484c5267808330eb5
Gilson Batista de Almeida Dourado <gilsondourado10@gmail.com>
2024-02-26 18:20:15 -0300

    feat(Metodos) adicionando método pagar

--- PagamentoDebito.cs
+++ PagamentoDebito.cs
@@ -3,22 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace Orientacao_objetos
+namespace Orientacao_objetos;
+
+public class PagamentoDebito : Pagamentos
+
 {
-    public class PagamentoDebito : Pagamentos
-    
+    public PagamentoDebito(DateTime vencimento, decimal valorPago, string nomeCliiente) : base(vencimento)
     {
-        public PagamentoDebito(DateTime vencimento, decimal valorPago, string nomeCliiente) : base(vencimento)
-        {
-        }
-
-        public decimal ValorPago{get ; set;}
-        public string NomeCliente{get; set; }
     }
-    
-            public bool ClienteAtivo()
-            {
 
-            }
-    
+    public decimal ValorPago { get; set; }
+    public string NomeCliente { get; set; }
+
+    public override void Pagar()
+    {
+        Console.WriteLine("");
+    }
+
 }
+
+
